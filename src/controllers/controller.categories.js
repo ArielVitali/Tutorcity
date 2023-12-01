d051af983d58e3edd1ba11d05316e19ffec21d14
ArielVitali <arielgonzalezvitali@hotmail.com>
2023-12-01 10:09:03 -0300

    categories

--- src/controllers/controller.categories.js
+++ src/controllers/controller.categories.js
@@ -1,14 +1,34 @@
 import RouterClass from "../router/router.class.js";
 import {
   getCategories,
+  getCategoryById,
   createCategory,
+  getCategoryByName,
 } from "../services/categories.service.js";
 
 class CategoryRouter extends RouterClass {
   init() {
+    this.get("/name/:name", ["PUBLIC"], async (req, res) => {
+      try {
+        const response = await getCategoryByName(req.params.name);
+        res.sendSuccess(response);
+      } catch (error) {
+        res.sendServerError(`something went wrong ${error}`);
+      }
+    });
+
     this.get("/", ["PUBLIC"], async (req, res) => {
       try {
         const response = await getCategories();
+        res.sendSuccess(response);
+      } catch (error) {
+        res.sendServerError(`something went wrong ${error}`);
+      }
+    });
+
+    this.get("/:id", ["PUBLIC"], async (req, res) => {
+      try {
+        const response = await getCategoryById(req.params.id);
         res.sendSuccess(response);
       } catch (error) {
         res.sendServerError(`something went wrong ${error}`);
