9b4565ca61eb185f16849db3244b11a352fa0083
ArielVitali <arielgonzalezvitali@hotmail.com>
2023-12-06 12:13:42 -0300

    update

--- frontend/src/api/config/config.axios.js
+++ frontend/src/api/config/config.axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://tutorcity-backend-production.up.railway.app/api",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
